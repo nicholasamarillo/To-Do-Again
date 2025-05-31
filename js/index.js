d8ff9fa04bd02f521ac53a20b90e985bf9457378
Nicholas Amarillo <82684025+nicholasamarillo@users.noreply.github.com>
2025-05-31 11:10:14 -0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector('.button');
+const board = document.querySelector('.board');
+const tasks = [];
+
+
+btn.addEventListener('click', function(){
+    let taskName = prompt("Please enter the name of your task.")
+    // console.log(taskName, taskDetails);
+    tasks.push(taskName.trim());
+    renderTasks();
+});
+
+function renderTasks(){
+    board.innerHTML = '';
+
+    tasks.forEach((task, index) => {
+        const card = document.createElement('div');
+        card.className = 'task-card';
+        card.textContent = task;
+        const deleteBtn = document.createElement('button')
+        deleteBtn.className = 'delete';
+        deleteBtn.textContent = 'Completed';
+        board.appendChild(card);
+        card.appendChild(deleteBtn);
+
+        deleteBtn.addEventListener('click', function(){
+            tasks.splice(index, 1);
+            renderTasks();
+        })
+    });
+
+    
+}
